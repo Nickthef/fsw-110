f5ba773d663adc5edf5eac6056218c7241b275ab
Nickthef <nicolas.franks@bryanuniversity.edu>
2022-07-16 11:16:38 -0400

    week 5 modify elements

---
+++ week5/modifying-elements/index.js
@@ -0,0 +1,9 @@
+function alertInputValues() {
+  const firstName = document.getElementById("fname").value;
+  const LastName = document.getElementById("lname").value;
+  const middleName = document.getElementById("mname").value;
+
+  alert(firstName + " " + LastName + " " + middleName);
+}
+
+
